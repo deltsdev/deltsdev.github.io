0af90bc5f1f8037bcd7311445345c92b71014046
deltsdev <delthondev@gmail.com>
2022-10-02 21:30:52 +0800

    Fix

--- WhoUCreate/flutter_service_worker.js
+++ WhoUCreate/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e5e7d85f1c5630ea7a7995d3ba59098b",
-"/": "e5e7d85f1c5630ea7a7995d3ba59098b",
-"main.dart.js": "cf4d85e60dcd2e13f7fd2ca99047dd53",
+"index.html": "f5fd1edfbfb3d43f473ba53de4b5227f",
+"/": "f5fd1edfbfb3d43f473ba53de4b5227f",
+"main.dart.js": "ef1a7ab025b8dcc7c240e0cad936735d",
 "manifest.json": "a81fd2c0b82355e58829c7a84b647a8b",
 "version.json": "9e5e2302b9caf161189d87f1585f90e5"
 };
